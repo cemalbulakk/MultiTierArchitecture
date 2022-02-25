f1c5801e7498a1f3983826d9f831260ab77914e5
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-02-25 19:37:15 +0300

    Add project files.

---
+++ Core/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Core;
+
+public abstract class BaseEntity
+{
+    public string Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+}
---
+++ Core/ProductFeature.cs
@@ -0,0 +1,12 @@
+﻿namespace Core;
+
+public class ProductFeature
+{
+    public string Id { get; set; }
+    public string Color { get; set; }
+    public int Height { get; set; }
+    public int Width { get; set; }
+
+    public string ProductId { get; set; }
+    public Product Product { get; set; }
+}
