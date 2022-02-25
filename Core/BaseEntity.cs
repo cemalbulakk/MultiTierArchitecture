51602f341210b2853e30124e32f33cd0242725ca
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-02-25 19:48:27 +0300

    .Net6 nullable

--- Core/BaseEntity.cs
+++ Core/BaseEntity.cs
@@ -2,6 +2,12 @@
 
 public abstract class BaseEntity
 {
+    protected BaseEntity()
+    {
+        UpdatedDate = DateTime.Now;
+        Id = Guid.NewGuid().ToString("N");
+    }
+
     public string Id { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime UpdatedDate { get; set; }
