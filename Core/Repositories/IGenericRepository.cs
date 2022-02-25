942e60a9ccd730b8c43fcf1ce710a80e6973ae99
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-02-25 19:54:37 +0300

    18. IGenericRepository

---
+++ Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Repositories;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(string id);
+    IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);
+    IQueryable<T> GetWhere(Expression<Func<T, bool>> predicate);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
+    void Update(T entity);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entity);
+}
