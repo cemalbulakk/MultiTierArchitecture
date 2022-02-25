cd6df0e2da93378af990d872f3f33c2890ec95e2
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-02-25 20:47:48 +0300

    27. Service

--- Core/Services/IService.cs
+++ Core/Services/IService.cs
@@ -8,8 +8,8 @@ public interface IService<T> where T : class
     Task<IEnumerable<T>> GetAllAsync();
     IQueryable<T> GetWhere(Expression<Func<T, bool>> predicate);
     Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
-    Task AddAsync(T entity);
-    Task AddRangeAsync(IEnumerable<T> entities);
+    Task<T> AddAsync(T entity);
+    Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
     Task UpdateAsync(T entity);
     Task RemoveAsync(T entity);
     Task RemoveRangeAsync(IEnumerable<T> entity);
---
+++ Service/Services/Service.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq.Expressions;
+using Core.Repositories;
+using Core.Services;
+using Core.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service.Services;
+
+public class Service<T> : IService<T> where T : class
+{
+    private readonly IGenericRepository<T> _repository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<T> GetByIdAsync(string id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _repository.GetAll().ToListAsync();
+    }
+
+    public IQueryable<T> GetWhere(Expression<Func<T, bool>> predicate)
+    {
+        return _repository.GetWhere(predicate);
+    }
+
+    public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _repository.AnyAsync(predicate);
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _repository.AddAsync(entity);
+        await _unitOfWork.CommitAsync();
+        return entity;
+    }
+
+    public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+    {
+        await _repository.AddRangeAsync(entities);
+        await _unitOfWork.CommitAsync();
+        return entities;
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _repository.Update(entity);
+        await _unitOfWork.CommitAsync();
+
+    }
+
+    public async Task RemoveAsync(T entity)
+    {
+        _repository.Remove(entity);
+        await _unitOfWork.CommitAsync();
+    }
+
+    public async Task RemoveRangeAsync(IEnumerable<T> entity)
+    {
+        _repository.RemoveRange(entity);
+        await _unitOfWork.CommitAsync();
+    }
+}
